765c3e767f61206afcc21e7012d9815d07748404
Taelynn Knutson <taelynnknutson@Taelynns-MacBook-Pro.local>
2017-12-15 11:32:48 -0800

    beginning of primecheck

--- final_challenge_functions.js
+++ final_challenge_functions.js
@@ -1,3 +1,13 @@
+function checkPrime(number) {
+    for( i = 2; i <= number/2; i++){
+        if ((number)% i == 0) {
+            return "not prime";
+        }
+    }
+    return "prime";
+}
+
+
 function timesTable(number) {
 
     var rows = 10;
