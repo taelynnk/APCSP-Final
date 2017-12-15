d075c86752b73638675a0d51dcbbd9822afc3227
Taelynn A Knutson <tk62539@srcs.loc>
2017-12-15 10:41:24 -0800

    timesTable(i*j)

--- final_challenge_functions.js
+++ final_challenge_functions.js
@@ -1,13 +1,14 @@
-function timesTable(number)
+function timesTable(number) {
 
-var rows = 10;
-var cells = 10;
-document.write("<table>");
-for(i = 0; i < rows; i++) {
-    document.write("<tr>");
-    for(j = 0; j < cells; j++) {
-        document.write("<td>", i + j, "</td>");
+    var rows = 10;
+    var cells = 10;
+    document.write("<table>");
+    for (i = 0; i < rows; i++) {
+        document.write("<tr>");
+        for (j = 0; j < cells; j++) {
+            document.write("<td>", i * j, "</td>");
+        }
+        document.write("</tr>");
     }
-    document.write("</tr>");
+    document.write("</table>");
 }
-document.write("</table>");
