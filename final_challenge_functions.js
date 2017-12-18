4ff04e8f842c73d9cf019a731de91fc38a5f4fc7
Taelynn Knutson <taelynnknutson@Taelynns-MacBook-Pro.local>
2017-12-17 17:47:49 -0800

    final

--- final_challenge_functions.js
+++ final_challenge_functions.js
@@ -1,6 +1,12 @@
 function checkPrime(number) {
-    for( i = 2; i <= number/2; i++){
-        if ((number)% i == 0) {
+    if (number < 1) {
+        return "not prime"
+    }
+    if (number % 1 != 0) {
+        return "not prime"
+    }
+    for (i = 2; i <= number / 2; i++) {
+        else if ((number) % i == 0) {
             return "not prime";
         }
     }
@@ -10,13 +16,29 @@ function checkPrime(number) {
 
 function timesTable(number) {
 
-    var rows = 10;
-    var cells = 10;
+    var rows = number;
+    var cells = number;
     document.write("<table border=1>");
-    for (i = 1; i < rows; i++) {
+    for (i = 1; i <= rows; i++) {
         document.write("<tr>");
-        for (j = 1; j < cells; j++) {
+        for (j = 1; j <= cells; j++) {
             document.write("<td>", i * j, "</td>");
+        }
+        document.write("</tr>");
+    }
+    document.write("</table>");
+}
+
+
+function primeTable(number) {
+
+    var rows = number;
+    var cells = 10;
+   document.write("<table border=1>");
+    for (i = 0; i <= rows; i++) {
+        document.write("<tr>");
+        for (j = cells/10; j <= cells; ) {
+            document.write(Math.round(j), "<td>", i + j, "</td>");
         }
         document.write("</tr>");
     }
